827e9b3092aed0039c864d35389b32c77bc6669c
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-26 22:55:21 +0200

    build(backend): change mango protocol

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,7 @@
-import "dotenv/config";
+import 'dotenv/config'
 
-const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL } = process.env;
+const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL } = process.env
 
-const dbConnectionURL = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}`;
+const dbConnectionURL = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}`
 
-export default dbConnectionURL;
+export default dbConnectionURL
