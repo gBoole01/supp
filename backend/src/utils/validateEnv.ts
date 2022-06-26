1b7885bf7af9907765255c30c34f946a02a7f36a
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-26 22:56:07 +0200

    build: add new env variable

--- backend/src/utils/validateEnv.ts
+++ backend/src/utils/validateEnv.ts
@@ -1,12 +1,13 @@
-import { cleanEnv, port, str } from "envalid";
-import "dotenv/config";
+import { cleanEnv, port, str } from 'envalid'
+import 'dotenv/config'
 
 export default function validateEnv() {
   cleanEnv(process.env, {
+    NODE_ENV: str(),
     PORT: port(),
     MONGODB_USER: str(),
     MONGODB_PASSWORD: str(),
     MONGODB_URL: str(),
     TOKEN_SECRET_KEY: str(),
-  });
+  })
 }
