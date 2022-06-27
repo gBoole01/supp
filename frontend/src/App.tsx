5ab78734b4041abd19db0a1324f3edb1d42d4a42
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-27 17:22:41 +0200

    feat(frontend): implement ApolloClient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,33 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import './App.css'
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        tasks: {
+          merge(_existing, incoming) {
+            return incoming
+          },
+        },
+      },
+    },
+  },
+})
+
+const client = new ApolloClient({
+  uri: import.meta.env.GRAPHQL_BASE_ROUTE,
+  cache,
+})
+
 const App = () => (
-  <div className="App">
-    <h1>Hello World !</h1>
-  </div>
+  <>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <h1 className="h1">Hello World !</h1>
+      </div>
+    </ApolloProvider>
+  </>
 )
 
 export default App
