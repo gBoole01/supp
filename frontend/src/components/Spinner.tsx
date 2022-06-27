2eabf51dfdec57f3c3c42817bb4e491200246fd1
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-27 19:39:14 +0200

    feat(frontend): add Spinner component

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => (
+  <div className="d-flex justify-content-center">
+    <div className="spinner-broder">
+      <span className="sr-only"></span>
+    </div>
+  </div>
+)
+
+export default Spinner
