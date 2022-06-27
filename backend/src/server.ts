06e84d94fa8adc27996124bc9b33d5b64b47a417
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-27 15:41:34 +0200

    refactor: remove unused parts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
-import validateEnv from './utils/validateEnv'
+import validateEnv from './lib/validateEnv'
 import App from './app'
 
 validateEnv()
 
-const app = new App([])
+const app = new App()
 
 app.connectToDatabase().then(() => {
   app.listen()
