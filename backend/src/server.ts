71e7b1154261e29e559b8e4f413426afa965fbb4
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-28 16:25:07 +0200

    feat(backend): implement AuthenticationController

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import validateEnv from './lib/validateEnv'
 import App from './app'
+import AuthenticationController from './services/authentication/authentication.controller'
+import Logger from './lib/logger'
 
+Logger.info(`🚀 App starting in ${process.env.NODE_ENV} mode ! Warming up...`)
 validateEnv()
 
-const app = new App()
+const app = new App([new AuthenticationController()])
 
 app.connectToDatabase().then(() => {
   app.listen()
---
+++ backend/src/services/authentication/authentication.controller.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response, Router } from 'express'
+import Controller from '../../interfaces/Controller.interface'
+import AuthenticationService from './authentication.service'
+import validationMiddleware from '../../middlewares/validation.middleware'
+import CreateUserDTO from './CreateUserDTO'
+
+class AuthenticationController implements Controller {
+  public path = '/auth'
+
+  public router = Router()
+
+  private authenticationService = new AuthenticationService()
+
+  constructor() {
+    this.initializeRoutes()
+  }
+
+  private initializeRoutes() {
+    this.router.post(
+      `${this.path}/register`,
+      validationMiddleware(CreateUserDTO),
+      AuthenticationController.registration,
+    )
+  }
+
+  private static registration = async (
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) => {
+    const userData: CreateUserDTO = request.body
+    try {
+      await AuthenticationService.register(userData)
+      response.sendStatus(200)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export default AuthenticationController
