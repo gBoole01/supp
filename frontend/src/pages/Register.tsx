201de32a11e2cbbee75c6f4162be225493023b6c
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-29 18:13:29 +0200

    feat(frontend): implement Register form validation

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,24 +1,71 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
 
-  const handleValidation = () => true
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    emailFormat: false,
+    password: false,
+    passwordConfirm: false,
+  })
 
-  const handleSubmit = async (e: SubmitEvent) => {
-    e.preventDefault()
-    console.log('handleSubmit')
+  const handleValidation = () => {
+    const tempErrors = {
+      name: false,
+      email: false,
+      emailFormat: false,
+      password: false,
+      passwordConfirm: false,
+    }
+    let isValid = true
+
+    if (name.length <= 0) {
+      tempErrors.name = true
+      isValid = false
+    }
+
+    if (email.length <= 0) {
+      tempErrors.email = true
+      isValid = false
+    }
+
+    if (!email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+      tempErrors.emailFormat = true
+      isValid = false
+    }
+
+    if (password.length <= 5) {
+      tempErrors.password = true
+      isValid = false
+    }
+
+    if (password !== passwordConfirm) {
+      tempErrors.passwordConfirm = true
+      isValid = false
+    }
+
+    setErrors({ ...tempErrors })
+    return isValid
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
     const isValidForm = handleValidation()
     if (isValidForm) {
-      const response = await fetch('https://localhost:5000/auth/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, email, password }),
-      })
-
-      console.log(response)
+      // const response = await fetch('https://localhost:5000/auth/register', {
+      //   method: 'POST',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   body: JSON.stringify({ name, email, password }),
+      // })
+      setName('')
+      setEmail('')
+      setPassword('')
+      setPasswordConfirm('')
     }
   }
 
@@ -26,7 +73,7 @@ const Register = () => {
     <>
       <form
         className="d-flex flex-column gap-4 align-items-center"
-        onSubmit={() => handleSubmit}
+        onSubmit={handleSubmit}
         id="register-form"
       >
         <div className="form-group">
@@ -36,22 +83,25 @@ const Register = () => {
           <input
             type="text"
             className="form-control"
-            id="register-name"
+            id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
+          {errors?.name && <p>This field is empty</p>}
         </div>
         <div className="form-group">
           <label className="form-label" htmlFor="email">
             Email
           </label>
           <input
-            type="email"
+            type="text"
             className="form-control"
-            id="register-email"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {errors?.email && <p>This field is empty</p>}
+          {!errors?.email && errors?.emailFormat && <p>Wrong mail format</p>}
         </div>
         <div className="form-group">
           <label className="form-label" htmlFor="password">
@@ -60,11 +110,26 @@ const Register = () => {
           <input
             type="password"
             className="form-control"
-            id="register-password"
+            id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {errors?.password && <p>Password length must be 6+ char.</p>}
         </div>
+        <div className="form-group">
+          <label className="form-label" htmlFor="passwordConfirm">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="passwordConfirm"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+          {errors?.passwordConfirm && <p>Please enter the same password</p>}
+        </div>
+
         <button type="submit" className="btn btn-secondary">
           Register
         </button>
