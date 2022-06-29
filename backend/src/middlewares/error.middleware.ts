8b5aef92ce5aceb64c8dea1fa3e13ebbedf9dcf3
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-29 14:14:39 +0200

    feat(backend): add Logger instructions in error.middleware

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,14 +1,18 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
+import Logger from '../lib/logger'
 import HTTPException from '../exceptions/HTTPException'
 
 function errorMiddleware(
   error: HTTPException,
   _request: Request,
   response: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
 ) {
   const status = error.status || 500
   const message = error.message || 'Something went wrong'
-  response.status(status).send(message)
+  Logger.error(`⛔ Error ${status}: ${message}`)
+  response.status(status).send({ status, message })
 }
 
 export default errorMiddleware
