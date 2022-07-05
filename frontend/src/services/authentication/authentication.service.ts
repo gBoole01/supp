faca4cab8d910daa42573ca51ebf98d5325ef05a
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-07-05 11:50:29 +0200

    feat(frontend): implement AuthenticationService

---
+++ frontend/src/services/authentication/authentication.service.ts
@@ -0,0 +1,81 @@
+import axios from 'axios'
+
+const AUTHENTICATION_ENDPOINT = `${import.meta.env.VITE_API_URL}/auth`
+
+type RegisterResponse = {
+  name: string
+  email: string
+  password: string
+  _id: string
+}
+
+type LoginResponse = {
+  name: string
+  email: string
+  password: string
+  _id: string
+}
+
+class AuthenticationService {
+  static async register(name: string, email: string, password: string) {
+    try {
+      const { data } = await axios.post<RegisterResponse>(
+        `${AUTHENTICATION_ENDPOINT}/register`,
+        {
+          name,
+          email,
+          password,
+        },
+      )
+
+      return {
+        data,
+        error: null,
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(`Axios Error: ${error.message}`)
+        return {
+          data: null,
+          error: error.message,
+        }
+      }
+      console.error(`Unexpected Error: ${error}`)
+      return {
+        data: null,
+        error: 'Something went wrong',
+      }
+    }
+  }
+
+  static async login(email: string, password: string) {
+    try {
+      const { data } = await axios.post<LoginResponse>(
+        `${AUTHENTICATION_ENDPOINT}/login/password`,
+        {
+          email,
+          password,
+        },
+      )
+      return {
+        data,
+        error: null,
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(`Axios Error: ${error.message}`)
+        return {
+          data: null,
+          error: error.message,
+        }
+      }
+      console.error(`Unexpected Error: ${error}`)
+      return {
+        data: null,
+        error: 'Something went wrong',
+      }
+    }
+  }
+}
+
+export default AuthenticationService
