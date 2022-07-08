1d39abe53a11d95c352dda4d3b88271713bf4b5b
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-07-08 11:42:58 +0200

    feat(backend): add getToken() and getRefreshToken() ; integrate  new mongoose Model

--- backend/src/services/authentication/authentication.service.ts
+++ backend/src/services/authentication/authentication.service.ts
@@ -1,25 +1,69 @@
-import { hash } from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
 import User from '../../models/User'
 import Logger from '../../lib/logger'
 import CreateUserDTO from './CreateUserDTO'
-import EmailAlreadyInUseException from '../../exceptions/EmailAlreadyInUseException'
+import { UserI } from '../../interfaces/User.interface'
 
 class AuthenticationService {
   public static async register(userData: CreateUserDTO) {
-    const existingUser = await User.findOne({ email: userData.email })
-    if (existingUser) {
-      throw new EmailAlreadyInUseException(userData.email)
-    }
+    const user = await User.register(
+      new User({ username: userData.email }),
+      userData.password,
+    )
+    user.firstName = userData.name
 
-    const hashedPassword = await hash(userData.password, 10)
-    const user = new User({
-      ...userData,
-      password: hashedPassword,
-    })
+    const token = AuthenticationService.getToken(user)
+    const refreshToken = AuthenticationService.getRefreshToken(user)
+    user.refreshToken.push({ refreshToken })
+
     user.save()
-    Logger.info(`📢 ${user.name} just created an account ! 🎉`)
+    Logger.info(`📢 ${user.firstName} just created an account ! 🎉`)
 
-    return user
+    return { token, refreshToken }
+  }
+
+  // public static async authenticate(user: Express.User) {
+  //   const dbUser = await User.findById(user)
+  //   console.log(user)
+  //   console.log(dbUser)
+  //   const token = AuthenticationService.getToken(dbUser)
+  //   const refreshToken = AuthenticationService.getRefreshToken(dbUser)
+
+  //   return { token, refreshToken }
+  // }
+
+  private static getToken(user: UserI) {
+    const {
+      JWT_SECRET,
+      SESSION_EXPIRACY_SECONDS: seconds,
+      SESSION_EXPIRACY_MINUTES: minutes,
+    } = process.env
+
+    const expiresIn = parseInt(seconds, 10) * parseInt(minutes, 10)
+
+    return jwt.sign(user.toJSON(), JWT_SECRET, {
+      expiresIn,
+    })
+  }
+
+  private static getRefreshToken(user: UserI) {
+    const {
+      JWT_SECRET,
+      REFRESH_TOKEN_EXPIRACY_SECONDS: seconds,
+      REFRESH_TOKEN_EXPIRACY_MINUTES: minutes,
+      REFRESH_TOKEN_EXPIRACY_HOURS: hours,
+      REFRESH_TOKEN_EXPIRACY_DAYS: days,
+    } = process.env
+
+    const expiresIn =
+      parseInt(seconds, 10) *
+      parseInt(minutes, 10) *
+      parseInt(hours, 10) *
+      parseInt(days, 10)
+
+    return jwt.sign(user.toJSON(), JWT_SECRET, {
+      expiresIn,
+    })
   }
 }
 
