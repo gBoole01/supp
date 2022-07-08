3a8f5cf9e76c359b3bc017b9a2a50c25634c2745
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-07-08 11:36:55 +0200

    feat(backend): define cookie config

---
+++ backend/src/config/cookie.ts
@@ -0,0 +1,25 @@
+import { CookieOptions } from 'express'
+
+const {
+  REFRESH_TOKEN_EXPIRACY_SECONDS: seconds,
+  REFRESH_TOKEN_EXPIRACY_MINUTES: minutes,
+  REFRESH_TOKEN_EXPIRACY_HOURS: hours,
+  REFRESH_TOKEN_EXPIRACY_DAYS: days,
+} = process.env
+
+const maxAge =
+  parseInt(seconds, 10) *
+  parseInt(minutes, 10) *
+  parseInt(hours, 10) *
+  parseInt(days, 10) *
+  1000
+
+const cookieConfig: CookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV !== 'production',
+  signed: true,
+  maxAge,
+  sameSite: 'none',
+}
+
+export default cookieConfig
