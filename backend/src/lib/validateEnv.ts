901007f54c7916d9fb8b9a3a2fa71ff9e7e63f27
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-07-08 11:38:26 +0200

    feat(backend): add new env variables

--- backend/src/lib/validateEnv.ts
+++ backend/src/lib/validateEnv.ts
@@ -1,4 +1,4 @@
-import { cleanEnv, port, str } from 'envalid'
+import { cleanEnv, num, port, str, url } from 'envalid'
 import 'dotenv/config'
 import Logger from './logger'
 
@@ -7,12 +7,18 @@ export default function validateEnv() {
   cleanEnv(process.env, {
     NODE_ENV: str(),
     PORT: port(),
+    REACT_APP_DOMAIN: url(),
+    COOKIE_SECRET: str(),
     MONGODB_USER: str(),
     MONGODB_PASSWORD: str(),
     MONGODB_URL: str(),
-    AUTH0_DOMAIN: str(),
-    AUTH0_CLIENT_ID: str(),
-    AUTH0_CLIENT_SECRET: str(),
+    JWT_SECRET: str(),
+    REFRESH_TOKEN_EXPIRACY_SECONDS: num(),
+    REFRESH_TOKEN_EXPIRACY_MINUTES: num(),
+    REFRESH_TOKEN_EXPIRACY_HOURS: num(),
+    REFRESH_TOKEN_EXPIRACY_DAYS: num(),
     SESSION_SECRET: str(),
+    SESSION_EXPIRACY_SECONDS: num(),
+    SESSION_EXPIRACY_MINUTES: num(),
   })
 }
