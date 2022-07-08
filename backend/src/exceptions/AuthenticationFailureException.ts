d2f31a04e7d757d65522345567c5e17b7ee0d664
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-07-08 15:48:33 +0200

    feat(backend): remove unused exceptions

--- backend/src/exceptions/AuthenticationFailureException.ts
+++
@@ -1,7 +0,0 @@
-import HTTPException from './HTTPException'
-
-export default class AuthenticationFailureException extends HTTPException {
-  constructor() {
-    super(500, `Something went wrong during authentication`)
-  }
-}
