96c36ade79dd336eda98a21dfd5c487cc095fa28
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-07-09 21:47:55 +0200

    feat(frontend): add UserDetails component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import AddTaskModal from '../components/AddTaskModal'
 import Tasks from '../components/Tasks'
+import UserDetails from '../components/UserDetails'
 
 const Home = () => (
   <>
     <AddTaskModal />
     <Tasks />
+    <UserDetails />
   </>
 )
 export default Home
